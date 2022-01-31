6f72b2e16f14e3cc597a661c4eb5d9d6f09eab43
Matheus Tanaka <matheustanakanog@gmail.com>
2022-01-31 19:23:34 -0300

    feat: connected to database

--- src/database/server.js
+++ src/database/server.js
@@ -1,19 +1,19 @@
 import { ApolloServer } from 'apollo-server'
 import mongoose from 'mongoose'
 
-function startServer ({ typeDefs, resolvers }) {
-//   mongoose.connect(
-//     `mongodb+srv://${process.env.NODE_USERNAME}:${process.env.NODE_PASSWORD}@drugs-wallet.igbzq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//     );
+const startServer = ({ typeDefs, resolvers }) => {
+  mongoose.connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@drugs-wallet.igbzq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+    );
   
-//   //Conecting database with mongoose
-//   const db = mongoose.connection
-//   db.on('error', (error) => console.error(error))
-//   db.once('open', () => console.log('Connected to the database'))
+  //Conecting database with mongoose
+  const db = mongoose.connection
+  db.on('error', (error) => console.error(error))
+  db.once('open', () => console.log('Connected to the database'))
 
   const server = new ApolloServer({ typeDefs, resolvers })
   server
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import startServer from "./database/server";
 import typeDefs from "./graphql/typeDefs";
 import resolvers from "./graphql/resolvers";
+import dotenv from "dotenv";
 
+dotenv.config();
 startServer({ typeDefs, resolvers});
