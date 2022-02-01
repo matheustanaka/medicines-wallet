9e0a4c507f8e82dfbc5498968391c95836a96302
Matheus Tanaka <matheustanakanog@gmail.com>
2022-02-01 17:59:52 -0300

    feat: add mergeResolvers

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,1 +1,12 @@
-export default {};
+import { mergeResolvers } from '@graphql-tools/merge'
+import { loadFilesSync } from '@graphql-tools/load-files'
+import path from "path";
+
+//Adding a path that find all those files "modules", "**", "resolvers.js"
+const resolversArray = loadFilesSync(
+  path.join(__dirname, "modules", "**", "resolvers.js")
+);
+const resolvers = mergeResolvers(resolversArray);
+
+
+export default resolvers;
