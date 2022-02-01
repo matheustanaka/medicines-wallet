20ed494ec35b2448957430cc4b8565510aa0e889
Matheus Tanaka <matheustanakanog@gmail.com>
2022-02-01 17:59:41 -0300

    feat: add mergeTypeDefs

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -1,5 +1,11 @@
-export default `
-    type Query {
-        hello: String
-    }
-`;
+import { mergeTypeDefs } from '@graphql-tools/merge'
+import { loadFilesSync } from '@graphql-tools/load-files'
+import path from "path";
+
+//Adding a path that find all those files "modules", "**", "*.gql"
+const typesArray = loadFilesSync(path.join(__dirname, "modules", "**", "*.gql"));
+const typeDefs = mergeTypeDefs(typesArray);
+
+//Return the typeDefs with type Users and Drugs
+
+export default typeDefs;
