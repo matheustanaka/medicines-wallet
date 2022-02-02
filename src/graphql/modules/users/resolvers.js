169970f906daa74af3b6cc49f0c073827c125988
Matheus Tanaka <matheustanakanog@gmail.com>
2022-02-02 17:06:47 -0300

    fix: add async await to query and mutations

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -2,16 +2,16 @@ import User from '../../../models/User'
 
 export default {
   User: {
-    name: (user) => `${user.name}`
+    name: async (user) => await `${user.name}`
   },
   Query: {
-    users: () => User.find(),
-    user: (_, { id }) => User.findById(id)
+    users: async () => await User.find(),
+    user: async (_, { id }) => await User.findById(id)
   },
   Mutation: {
-    createUser: (_, { data }) => User.create(data),
-    updateUser: (_, { id, data }) =>
-      User.findByIdAndUpdate(id, data, { new: true }),
+    createUser: async (_, { data }) => await User.create(data),
+    updateUser: async (_, { id, data }) =>
+      await User.findByIdAndUpdate(id, data, { new: true }),
     deleteUser: async (_, { id }) => !!(await User.findByIdAndDelete(id))
   }
 }
